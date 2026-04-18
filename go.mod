8c0d001e92a31f9cb7dd4e3be15b5e7a8c074798
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2026-04-18 15:33:18 +0530

    intial commit server to 1 communication

---
+++ go.mod
@@ -0,0 +1,8 @@
+module chatonetoone
+
+go 1.25.4
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.3
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+const WELCOME_MESSAGE = "<h1>welcome to the chat application<h1>"
+
+var con *websocket.Conn
+var err error
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+func main(){
+	r := mux.NewRouter()
+	r.HandleFunc("/",greetingMessage)
+	r.HandleFunc("/ws",upgradeToWebsocket)
+	log.Print("Listening on port 3000 ...")
+	if err := http.ListenAndServe(":3000",r); err != nil{
+		panic(err)
+	}
+}
+
+func greetingMessage(w http.ResponseWriter, req *http.Request){
+	fmt.Fprintf(os.Stdout, "timestamp: %s | host: %s", time.Now().UTC().Format(time.RFC3339), req.Host)
+	welcomemsg := "<h1>welcome to the chat application<h1>"
+	w.WriteHeader(200)
+	w.Write(([]byte)(welcomemsg))
+}
+
+func upgradeToWebsocket(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("status","101")
+	con, err = upgrader.Upgrade(w,r,w.Header())
+	if(err != nil){
+		w.Write([]byte(err.Error()))
+	}
+	con.SetCloseHandler(
+		func(code int, text string) error {
+			log.Printf("connection closed: %d %s", code, text)
+			return nil
+		},
+	)
+	userinp := bufio.NewReader(os.Stdin)
+	for{
+		_, reader, commuerr := con.NextReader()
+		if(commuerr != nil){
+			log.Print(commuerr.Error())
+			break
+		}
+		msg, _  := io.ReadAll(reader)
+		log.Print((string)(msg))
+		fmt.Println("Any thing you wanted to talk? If yes type a message:")
+		fmt.Print("> ")
+		inpmsg, _ := userinp.ReadString('\n')
+		w,wrr := con.NextWriter(1)
+		if(wrr != nil){
+			log.Print(wrr.Error())
+			break
+		}
+		if(inpmsg != ""){
+			w.Write([]byte(inpmsg))
+			w.Close()
+		}
+	}
+	log.Print("Successfully upgraded to wesocket connecton!")
+	w.WriteHeader(400)
+}
