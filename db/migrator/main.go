fe69f6f2ae10efb2b44ed637c4ce396270528982
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2026-04-25 12:36:16 +0530

    feat: added mode for generating migration file and handled versioning

--- db/migrator/main.go
+++ db/migrator/main.go
@@ -5,6 +5,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/goccy/go-yaml"
 	"github.com/golang-migrate/migrate/v4"
@@ -12,55 +15,122 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type DatabaseConfiguration struct{
+type DatabaseConfiguration struct {
 	Development map[string]any `yaml:"development"`
-	Test map[string]any `yaml:"test"`
-	Production map[string]any `yaml:"production"`
+	Test        map[string]any `yaml:"test"`
+	Production  map[string]any `yaml:"production"`
 }
 
-// required flags for environemt
-const(
+const (
 	DEVELOPMENT int = iota + 1
 	TEST
 	PRODUCTION
 )
 
-func main(){
+const migrationsDir = "db/migrator/migrations"
+const versionFile = migrationsDir + "/.version"
+
+func main() {
 	dbconfig := readDatabaseConfiguration()
 	flags := takeInputFlags()
+	
+	action := *flags["action"].(*string)
 	envVal := *flags["environment"].(*int)
 	currentenv := getAppropriateDbConfig(dbconfig, envVal)
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-	currentenv["username"],currentenv["password"], currentenv["host"], currentenv["port"],currentenv["database"])
-	db, err := sql.Open("mysql",dsn)
-	if(err != nil){
+
+	switch action {
+	case "run":
+		direction := *flags["direction"].(*string)
+		performMigration(currentenv, direction)
+	case "create":
+		tableName := *flags["table"].(*string)
+		generateMigrationFile(currentenv, tableName)
+	default:
+		fmt.Println("Invalid action. Use -action=run or -action=create")
+	}
+}
+
+func performMigration(currentenv map[string]any, direction string) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		currentenv["username"], currentenv["password"], currentenv["host"], currentenv["port"], currentenv["database"])
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
 		panic(err)
 	}
-	mysqldriver, errmysql := mysql.WithInstance(db,&mysql.Config{})
-	if (errmysql != nil){
-		panic(errmysql)
-	}
-	migrator, migerror := migrate.NewWithDatabaseInstance("file://db/migrator/migrations","mysql",mysqldriver)
-	if(migerror != nil){
-		panic(migerror)
+	defer db.Close()
+
+	driver, _ := mysql.WithInstance(db, &mysql.Config{})
+	m, err := migrate.NewWithDatabaseInstance("file://"+migrationsDir, "mysql", driver)
+	if err != nil {
+		panic(err)
 	}
 
-	dir := *flags["direction"].(*string)
-	if(dir == "up"){
-		migrator.Up()
+	if direction == "up" {
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			panic(err)
+		}
+	} else {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			panic(err)
+		}
 	}
-	if(dir == "down"){
-		migrator.Down()
+	fmt.Printf("Migration %s finished successfully\n", direction)
+}
+
+func generateMigrationFile(currentenv map[string]any, tableName string) {
+	if tableName == "" {
+		fmt.Println("Error: -table flag is required for creating migrations")
+		return
 	}
+
+	version := getLatestVersion(currentenv) + 1
+	upFileName := fmt.Sprintf("%06d_%s.up.sql", version, tableName)
+	downFileName := fmt.Sprintf("%06d_%s.down.sql", version, tableName)
+
+	os.Create(filepath.Join(migrationsDir, upFileName))
+	os.Create(filepath.Join(migrationsDir, downFileName))
+
+	// Update local tracker
+	os.WriteFile(versionFile, []byte(strconv.Itoa(version)), 0644)
+
+	fmt.Printf("Generated: %s and %s\n", upFileName, downFileName)
+}
+
+func getLatestVersion(currentenv map[string]any) int {
+	// 1. Try local file
+	data, err := os.ReadFile(versionFile)
+	if err == nil {
+		v, _ := strconv.Atoi(strings.TrimSpace(string(data)))
+		return v
+	}
+
+	// 2. Fallback to Database
+	fmt.Println("Local version file missing, checking database...")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		currentenv["username"], currentenv["password"], currentenv["host"], currentenv["port"], currentenv["database"])
+	
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return 0
+	}
+	defer db.Close()
+
+	var version int
+	err = db.QueryRow("SELECT version FROM schema_migrations ORDER BY version DESC LIMIT 1").Scan(&version)
+	if err != nil {
+		return 0
+	}
+	return version
 }
 
 func readDatabaseConfiguration() *DatabaseConfiguration {
 	rawDbConfig, err := os.ReadFile("configs/databases/database.yml")
 	dbconfig := DatabaseConfiguration{}
-	if(err != nil){
+	if err != nil {
 		panic(err)
 	}
-	if errun := yaml.Unmarshal(rawDbConfig, &dbconfig); errun != nil{
+	if errun := yaml.Unmarshal(rawDbConfig, &dbconfig); errun != nil {
 		panic(errun)
 	}
 	return &dbconfig
@@ -68,11 +138,17 @@ func readDatabaseConfiguration() *DatabaseConfiguration {
 
 func takeInputFlags() map[string]any {
 	result := make(map[string]any)
-	env := flag.Int("env",1,"--env=1 or --env 1 is for selecting the right database environment for performing migrations. possible environemnts are 1 -> development, 2 -> test and 3 -> production")
-	direction := flag.String("direction","up","direction of the migration wether it has to be up or down. defaults to up if non provided")
+	env := flag.Int("env", 1, "environment: 1=dev, 2=test, 3=prod")
+	direction := flag.String("direction", "up", "migration direction: up/down")
+	action := flag.String("action", "run", "action: run/create")
+	table := flag.String("table", "", "table name with create, update and delete can be mentioned(eg:create_users_table)(only for action=create)")
+
 	flag.Parse()
+
 	result["environment"] = env
 	result["direction"] = direction
+	result["action"] = action
+	result["table"] = table
 	return result
 }
 
