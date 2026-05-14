2249213291a67b5a1f289f4c1480c535ad5acd96
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2026-05-14 21:06:48 +0530

    Feat: added user repository for find by email

---
+++ internals/repositories/users/sql_user_repository.go
@@ -0,0 +1,31 @@
+package users
+
+import (
+	"chatonetoone/internals/models"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type SqlUserRepository struct{
+	db *gorm.DB
+}
+
+func NewSqlUserRepository(db *gorm.DB) (*SqlUserRepository, error) {
+	if db == nil{
+		return nil, errors.New("argument should must contain db")
+	}
+	return &SqlUserRepository{db: db}, nil
+}
+
+func(ur *SqlUserRepository) FindUserByEmail(email string) (*models.User, error){
+	var user models.User
+	result := ur.db.Where(models.User{Email: email}).First(&user)
+	if(result.Error != nil){
+		if(errors.Is(result.Error, gorm.ErrRecordNotFound)){
+			return nil, errors.New("Record not found")
+		}
+		return nil, result.Error
+	}
+	return &user, nil
+}
