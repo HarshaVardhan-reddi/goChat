992034b863b100e5b87a12d838dd085ebb9168ca
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2026-05-24 16:49:09 +0530

    Refactor: delegate API v1 routing and clean up main.go

--- configs/router.go
+++ configs/router.go
@@ -1,17 +1,29 @@
 package configs
 
 import (
+	"chatonetoone/internals/controllers/v1/chats"
 	"chatonetoone/internals/controllers/v1/users"
+	user_repos "chatonetoone/internals/repositories/users_repos"
 
 	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
-func IntializeRoutes(userCtrl *users.UsersController) *mux.Router {
+func IntializeRoutes(mysqldb *gorm.DB) *mux.Router {
 	router := mux.NewRouter()
-	v1router := router.PathPrefix("/api/v1").Subrouter()
+	
+	setupV1(router.PathPrefix("/api/v1").Subrouter(), mysqldb)
+
+	return router
+}
+
+func setupV1(v1router *mux.Router, mysqldb *gorm.DB) {
 	userRouter := v1router.PathPrefix("/users").Subrouter()
 	
+	repo, _ := user_repos.NewSqlUserRepository(mysqldb)
+	userCtrl := users.NewUsersController(mysqldb, repo)
 	users.RegisterRouters(userCtrl, userRouter)
-	
-	return router
+
+	chatsCtrl, _ := chats.NewController()
+	chats.RegisterRouters(chatsCtrl, v1router)
 }
--- main.go
+++ main.go
@@ -1,34 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"chatonetoone/configs"
 	"chatonetoone/configs/databases"
-	"chatonetoone/internals/controllers/v1/users"
-	user_repos "chatonetoone/internals/repositories/users_repos"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"time"
 
-	"github.com/gorilla/websocket"
 	"gorm.io/gorm"
 	"github.com/joho/godotenv"
 )
 
-const WELCOME_MESSAGE = "<h1>welcome to the chat application<h1>"
-
-var con *websocket.Conn
 var err error
 
 var MysqlDB *gorm.DB
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
-}
 
 func main(){
 	godotenv.Load()
@@ -46,67 +31,11 @@ func main(){
 	// fmt.Printf("%v",obj)
 	fmt.Printf("%T\n",MysqlDB)
 	fmt.Println(MysqlDB.Config)
-
-	// Dependency Injection Setup
-	userRepo, errInRepo := user_repos.NewSqlUserRepository(MysqlDB)
-	if errInRepo != nil{
-		log.Fatal(errInRepo)
-	}
-	userCtrl := users.NewUsersController(MysqlDB, userRepo)
 	
-	router := configs.IntializeRoutes(userCtrl)
-
-	// You can add your greeting and websocket routes to the main router here
-	// router.HandleFunc("/", greetingMessage)
-	// router.HandleFunc("/ws", upgradeToWebsocket)
+	router := configs.IntializeRoutes(MysqlDB)
 
 	log.Print("Listening on port 3000 ...")
 	if err := http.ListenAndServe(":3000", router); err != nil{
 		panic(err)
 	}
-}
-
-func greetingMessage(w http.ResponseWriter, req *http.Request){
-	fmt.Fprintf(os.Stdout, "timestamp: %s | host: %s", time.Now().UTC().Format(time.RFC3339), req.Host)
-	welcomemsg := "<h1>welcome to the chat application<h1>"
-	w.WriteHeader(200)
-	w.Write(([]byte)(welcomemsg))
-}
-
-func upgradeToWebsocket(w http.ResponseWriter, r *http.Request){
-	w.Header().Add("status","101")
-	con, err = upgrader.Upgrade(w,r,w.Header())
-	if(err != nil){
-		w.Write([]byte(err.Error()))
-	}
-	con.SetCloseHandler(
-		func(code int, text string) error {
-			log.Printf("connection closed: %d %s", code, text)
-			return nil
-		},
-	)
-	userinp := bufio.NewReader(os.Stdin)
-	for{
-		_, reader, commuerr := con.NextReader()
-		if(commuerr != nil){
-			log.Print(commuerr.Error())
-			break
-		}
-		msg, _  := io.ReadAll(reader)
-		log.Print((string)(msg))
-		fmt.Println("Any thing you wanted to talk? If yes type a message:")
-		fmt.Print("> ")
-		inpmsg, _ := userinp.ReadString('\n')
-		w,wrr := con.NextWriter(1)
-		if(wrr != nil){
-			log.Print(wrr.Error())
-			break
-		}
-		if(inpmsg != ""){
-			w.Write([]byte(inpmsg))
-			w.Close()
-		}
-	}
-	log.Print("Successfully upgraded to wesocket connecton!")
-	w.WriteHeader(400)
 }
