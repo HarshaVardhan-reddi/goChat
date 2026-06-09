e63c5d9b3031bec8f12c9be74d9bb88f02cda445
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2026-06-09 09:06:11 +0530

    refactor(ws): update Broker channel to handle WsEvent

--- internals/ws/connection.go
+++ internals/ws/connection.go
@@ -1,6 +1,7 @@
 package ws
 
 import (
+	"chatonetoone/internals/models"
 	"errors"
 	"net/http"
 	"os"
@@ -13,13 +14,13 @@ type Identifier string
 type WsConnection struct{
 	ID Identifier
 	Conn *websocket.Conn
-	Broker chan string
+	Broker chan models.WsEvent
 }
 
 var wsUpgrader *websocket.Upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024, CheckOrigin: originCheck}
 
 func NewConnection(id Identifier, w http.ResponseWriter, r *http.Request) (*WsConnection, error) {
-	wsconn := WsConnection{ID: id, Broker: make(chan string, 1024)}
+	wsconn := WsConnection{ID: id, Broker: make(chan models.WsEvent, 1024)}
 	conn, err := wsUpgrader.Upgrade(w,r,nil)
 	if err != nil{
 		return nil,errors.New("upgrade request failed")
